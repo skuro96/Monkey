69e13429135ba5dd251b0228cc45798c06496cdf
Sho Kurosu <xsho0212@outlook.com>
2021-05-10 14:15:35 +0900

    add: 中置演算子の構文解析関数

--- ast/ast.go
+++ ast/ast.go
@@ -119,7 +119,6 @@ func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
 /* ---------- prefix ---------- */
-
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
@@ -134,6 +133,28 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+/* ---------- infix ---------- */
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
 	out.WriteString(")")
 
 	return out.String()
