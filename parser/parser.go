15107a4c971ca12a58771c8eb904ab1fed4c8a25
skuro96 <skurosu0212@gmail.com>
2021-05-17 20:34:42 +0900

    add: 丸括弧対応

--- parser/parser.go
+++ parser/parser.go
@@ -79,6 +79,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+
 	// curTokenとpeekTokenにトークンをセット
 	p.nextToken()
 	p.nextToken()
@@ -281,4 +283,15 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	return exp
 }
