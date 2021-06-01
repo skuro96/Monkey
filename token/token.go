086682191146e6d6a47edfd60b6dcc741daceb0f
Sho Kurosu <xsho0212@outlook.com>
2021-06-02 01:53:51 +0900

    add: string lexer

--- token/token.go
+++ token/token.go
@@ -3,63 +3,64 @@ package token
 type TokenType string
 
 type Token struct {
-    Type    TokenType
-    Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF     = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    // 識別子，リテラル
-    IDENT   = "IDENT"
-    INT     = "INT"
+	// 識別子，リテラル
+	IDENT  = "IDENT"
+	INT    = "INT"
+	STRING = "STRING"
 
-    // 演算子
-    ASSIGN      = "="
-    PLUS        = "+"
-    MINUS       = "-"
-    BANG        = "!"
-    ASTERISK    = "*"
-    SLASH       = "/"
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-    LT      = "<"
-    GT      = ">"
-    EQ      = "=="
-    NOT_EQ  = "!="
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
 
-    // デリミタ
-    COMMA       = ","
-    SEMICOLON   = ";"
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
 
-    LPAREN  = "("
-    RPAREN  = ")"
-    LBRACE  = "{"
-    RBRACE  = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    // キーワード
-    FUNCTION    = "FUNCTION"
-    LET         = "LET"
-    TRUE        = "TRUE"
-    FALSE       = "FALSE"
-    IF          = "IF"
-    ELSE        = "ELSE"
-    RETURN      = ""
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = ""
 )
 
-var keywords = map[string]TokenType {
-    "fn": FUNCTION,
-    "let": LET,
-    "true": TRUE,
-    "false": FALSE,
-    "if": IF,
-    "else": ELSE,
-    "return": RETURN,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-    if tok, ok := keywords[ident]; ok {
-        return tok
-    }
-    return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
