cd1bf6fca4057480c9623a048bced7378cf79b7d
Sho Kurosu <xsho0212@outlook.com>
2021-06-03 01:54:42 +0900

    add: builtin function (len)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -252,11 +252,15 @@ func isTruthy(obj object.Object) bool {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
@@ -273,21 +277,34 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 }
 
 func applyFunction(fn object.Object, args []object.Object, name string) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	// function, ok := fn.(*object.Function)
+	// if !ok {
+	// 	return newError("not a function: %s", fn.Type())
+	// }
+
+	// if len(function.Parameters) > len(args) {
+	// 	return newError("too few arguments in call to '%s'", name)
+	// }
+	// if len(function.Parameters) < len(args) {
+	// 	return newError("too many arguments in call to '%s'", name)
+	// }
+
+	// extendedEnv := extendFunctionEnv(function, args)
+	// evaluated := Eval(function.Body, extendedEnv)
+	// return unwrapReturnValue(evaluated)
+
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+
+	case *object.Builtin:
+		return fn.Fn(args...)
+
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	if len(function.Parameters) > len(args) {
-		return newError("too few arguments in call to '%s'", name)
-	}
-	if len(function.Parameters) < len(args) {
-		return newError("too many arguments in call to '%s'", name)
-	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
