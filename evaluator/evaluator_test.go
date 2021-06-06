58c2db08ffebfd5bbe712ee5afb8591f6ea08028
Sho Kurosu <xsho0212@outlook.com>
2021-06-07 01:19:03 +0900

    add: builtin func 'push'

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -370,8 +370,8 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`last(1)`, "argument to `last` must be ARRAY, got INTEGER"},
 		{`rest([1, 2, 3])`, []int{2, 3}},
 		{`rest([])`, nil},
-		// {`push([], 1)`, []int{1}},
-		// {`push(1, 1)`, "argument to `push` must be ARRAY, got INTEGER"},
+		{`push([], 1)`, []int{1}},
+		{`push(1, 1)`, "argument to `push` must be ARRAY, got INTEGER"},
 		// {`puts("hello", "world!")`, nil},
 	}
 
