a31c54acefd92e125d972934b9fef223325087e0
Sho Kurosu <xsho0212@outlook.com>
2021-06-19 02:35:04 +0900

    add: ファイルから実行

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 	"monkey/repl"
 	"os"
 	"os/user"
 )
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) > 0 {
+		bytedata, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			io.WriteString(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		data := string(bytedata)
+		exec(data, os.Stdout)
+
+		return
+	}
+
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
@@ -15,4 +38,34 @@ func main() {
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func exec(input string, out io.Writer) {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		printParserErrors(out, p.Errors())
+		return
+	}
+
+	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
+
+	evaluator.DefineMacros(program, macroEnv)
+	expanded := evaluator.ExpandMacros(program, macroEnv)
+
+	evaluated := evaluator.Eval(expanded, env)
+	if evaluated != nil {
+		io.WriteString(out, evaluated.Inspect())
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
